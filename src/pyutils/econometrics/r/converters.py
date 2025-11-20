3fe0e0e7aa120d8de64f8d7a15b9d19c14f51879
Marc de la Barrera <marc.delabarrera@gmail.com>
2025-11-20 12:54:05 +0100

    na

---
+++ src/pyutils/econometrics/r/converters.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from rpy2 import robjects as ro
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+
+def python_to_r(data: pd.DataFrame):
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        r_obj = ro.conversion.py2rpy(data)
+    return r_obj
+
+def r_to_python(r_obj):
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        py_data = ro.conversion.rpy2py(r_obj)
+    return py_data
