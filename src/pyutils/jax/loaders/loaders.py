1b8f784b078e18a3937a7145a5b31d4222c432a1
Marc de la Barrera <marc.delabarrera@gmail.com>
2025-12-02 09:39:38 +0100

    loaders

--- src/pyutils/jax/loaders/loaders.py
+++ src/pyutils/jax/loaders/loaders.py
@@ -1,7 +1,10 @@
+import json
 import pickle
+import jax
 import jax.numpy as jnp
 import numpy as np
 from jax import Array
+from pathlib import Path
 
 def to_numpy(obj):
     if isinstance(obj, dict):
@@ -17,12 +20,42 @@ def to_jax(obj):
         return jnp.array(obj)
     return obj
 
-def save_dict(path, data):
-    data_numpy = to_numpy(data)
-    with open(path, "wb") as f:
-        pickle.dump(data_numpy, f)
+# def save_dict(path, data):
+#     data_numpy = to_numpy(data)
+#     with open(path, "wb") as f:
+#         pickle.dump(data_numpy, f)
 
 def load_dict(path):
     with open(path, "rb") as f:
         data_numpy = pickle.load(f)
     return to_jax(data_numpy)
+
+def save_pytree(path:Path, pytree:dict, overwrite:bool=False):
+    """
+    Saves a pytree (dict of dicts of jax.Arrays) to a JSON file
+    """
+    path = Path(path) if isinstance(path, str) else path
+
+    if path.exists() and overwrite is False:
+        raise FileExistsError(f"File {path} already exists. Use overwrite=True to overwrite.")
+    
+    pytree = jax.tree_util.tree_map(
+                    lambda x: x.tolist() if isinstance(x, Array) else x,
+                    pytree)
+    
+    with open(path, "w") as f:
+        json.dump(pytree, f, indent=4)
+
+
+def load_pytree(path: Path) -> dict:
+    """
+    Loads a pytree saved with save_pytree, converting lists back to jnp.array.
+    """
+    with open(path, "r") as f:
+        pytree = json.load(f)
+
+    def to_array(x):
+        # Lists become jnp.array, everything else stays as is
+        return jnp.array(x) if isinstance(x, list) else x
+
+    return jax.tree_util.tree_map(to_array, pytree)
