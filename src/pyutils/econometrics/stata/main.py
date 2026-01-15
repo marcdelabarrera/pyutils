91690490f790fc2478a8a9984aae65ef6d638c37
Marc de la Barrera <marc.delabarrera@gmail.com>
2026-01-15 15:55:02 +0100

    create RegressionResult

--- src/pyutils/econometrics/stata/main.py
+++ src/pyutils/econometrics/stata/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from pystata import stata # type: ignore
 
+import numpy as np
+from scipy.stats import norm
+
 # def ivreg(reg:str, data:pd.DataFrame):
 #     stata.pdataframe_to_data(data, force=True)
 #     stata.run(reg)
@@ -17,10 +20,29 @@ def xtset(pvar):
     stata.run(f'xtreg y x1 x2, fe')
     return stata.get_ereturn()
 
-def reghdfe(depvar:str, exog:list[str], absorb:str, vce:str, data:pd.DataFrame):
+
+from dataclasses import dataclass
+@dataclass
+class RegressionResult:
+    coeftable:pd.DataFrame
+
+
+    @classmethod
+    def from_ereturn(cls, ereturn:dict):
+        coeftable = pd.DataFrame({"coef": ereturn["e(b)"].flatten(),
+                                  "se": np.diag(np.sqrt(ereturn["e(V)"])),
+                                  }, index = ereturn["e(indepvars)"].split())
+        coeftable["t-stat"] = coeftable["coef"] / coeftable["se"]
+
+        coeftable["p-value"] = 2 * (1 - norm.cdf(np.abs(coeftable["t-stat"])))
+
+        return cls(coeftable=coeftable)
+
+
+def reghdfe(depvar:str, exog:list[str], absorb:str, vce:str, data:pd.DataFrame) -> RegressionResult:
     stata.pdataframe_to_data(data, force=True)
     stata.run(f'reghdfe {depvar} {" ".join(exog)}, absorb({absorb}) vce({vce})')
-    return stata.get_ereturn()
+    return RegressionResult.from_ereturn(stata.get_ereturn())
 
 def install_reghdf():
     stata.run('ssc install require, replace')
